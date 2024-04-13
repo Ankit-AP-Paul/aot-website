6965a7bc589cb0fedea6319a0041bc0bc19e367b
Ankit Paul <ankitpaul894@gmail.com>
2024-04-13 21:10:19 +0530

    added dynamic routing to department page

--- app/page.js
+++ app/page.js
@@ -1,7 +1,19 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  const departments = ["CSE", "CSBS", "ME", "ECE", "EE", "EEE", "MCA"];
   return (
-    <h1 className="text-red-600 text-5xl" >Hello World</h1>
+    <div>
+      <h1 className="text-red-600 text-5xl pb-10">Home</h1>
+      <div>
+        {departments.map((department, index) => (
+          <div key={index}>
+            <Link href={`/department/${department}`}>
+              {<p className="text-2xl hover:text-red-600">{department}</p>}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
