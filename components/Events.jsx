4bb9549b59302028bbf2a2a2f18808099e18ed34
Moitreyo <cmoitreyo@gmail.com>
2024-05-01 22:49:30 +0530

    Selected card structure developed

--- components/Events.jsx
+++ components/Events.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useRef, useEffect } from "react";
-import { Plus_Jakarta_Sans } from "next/font/google";
+import { Inter, Montserrat, Roboto, Poppins } from "next/font/google";
+import "@/app/globalicons.css";
 
 import { LayoutGrid } from "./ui/layout-grid";
 import p1 from "@/app/data/images/p1.jpg";
@@ -8,7 +9,16 @@ import p2 from "@/app/data/images/p2.jpg";
 import p3 from "@/app/data/images/p3.jpg";
 import p4 from "@/app/data/images/p4.jpg";
 
-const inter = Plus_Jakarta_Sans({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] });
+const montserrat = Montserrat({ subsets: ["latin"] });
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
 
 function Events() {
   return (
@@ -62,9 +72,56 @@ const SkeletonThree = () => {
 };
 const SkeletonFour = () => {
   return (
-    <div>
-      <div className={`text-white text-center text-base ${inter.className}`}>
+    <div className="flex flex-col items-center">
+      <div
+        className={`text-white text-center text-4xl font-medium tracking-wide mb-5 uppercase ${montserrat.className}`}
+      >
         Techfiesta
+      </div>
+      <div className={`text-white text-center mb-8 ${inter.className}`}>
+        Perched high above the world, this house offers breathtaking views and a
+        unique living experience. It&apos;s a place where the sky meets home,
+        and tranquility is a way of life.
+      </div>
+      <table
+        className={`text-white text-center border-t-[0.5px] border-white rounded mb-8 mx-auto`}
+      >
+        <tbody>
+          <tr className="border-b-[0.5px] border-white">
+            <td className="px-6 py-4 border-r-[0.5px] border-white ">
+              <div className="material-symbols-outlined">calendar_month</div>
+            </td>
+            <td className={`px-6 ${poppins.className}`}>Tuesday, December 3</td>
+          </tr>
+          <tr className="border-b-[0.5px] border-white">
+            <td className="px-6 py-4 border-r-[0.5px] border-white ">
+              <div className="material-symbols-outlined">currency_rupee</div>
+            </td>
+            <td className={`px-6 ${poppins.className}`}>20 / member</td>
+          </tr>
+          <tr className="border-b-[0.5px] border-white">
+            <td className="px-6 py-4 border-r-[0.5px] border-white ">
+              <div className="material-symbols-outlined">numbers</div>
+            </td>
+            <td className={`px-6 ${poppins.className}`}>3 Rounds</td>
+          </tr>
+          <tr className="border-b-[0.5px] border-white">
+            <td className="px-6 py-4 border-r-[0.5px] border-white ">
+              <div className="material-symbols-outlined">how_to_reg</div>
+            </td>
+            <td className={`px-6 ${poppins.className}`}>
+              D-Block ground floor registration
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div className={`text-white ml-5 self-start ${inter.className}`}>
+        Register online : Link
+      </div>
+      <div
+        className={`text-white text-center ml-5 mt-8 mb-6 border-white border-2 rounded p-2 px-auto ${inter.className}`}
+      >
+        Download Brochure
       </div>
     </div>
   );
