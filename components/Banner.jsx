ce76cebc868ad007f7a4987b9084e309e9faee98
Ankit Paul <ankitpaul894@gmail.com>
2024-05-05 11:02:15 +0530

    banner modifications

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -3,16 +3,17 @@ import TextSlider from "@/components/TextSlider";
 
 export default function Banner() {
   return (
-    <div class="flex gap-5 ">
-      <div class="flex-none w-20">
+    <div className="flex gap-5 ml-20 mr-20">
+      <div className="flex-none w-20">
         <img src="assets/images/aot-logo.png" alt="logo" />
       </div>
-      <div class="flex-initial mt-1.5">
+      <div className="flex-initial mt-1.5">
         <TextSlider
           name="Academy of Technology"
           tagline="Transforming your vision into reality"
         />
       </div>
+      <div className="flex-none mt-1.5">Search</div>
     </div>
   );
 }
--- components/TextSlider.jsx
+++ components/TextSlider.jsx
@@ -21,7 +21,7 @@ const TextSlider = ({ name, tagline }) => {
 
   return (
     <Slider {...settings}>
-      <div className="text-xl  ">
+      <div className="text-xl ">
         <h3>{name}</h3>
       </div>
       <div className="text-lg">
